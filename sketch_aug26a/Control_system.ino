2905b35a05d4362c4ebec1d1e638f3bd7149d98d
Joy <joy.xu@students.mq.edu.au>
2025-09-02 14:27:48 +1000

    connect to wifi code

--- sketch_aug26a/Control_system.ino
+++ sketch_aug26a/Control_system.ino
@@ -1,1 +1,22 @@
 
+#include <WiFi.h>
+
+const char* ssid = "network_here"
+const char* password = "password_here"
+
+void setup() {
+  Serial.begin();
+  pinMode(5,OUTPUT); //may not need
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+}
