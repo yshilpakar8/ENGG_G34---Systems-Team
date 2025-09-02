4bafd4a29a7378c1fe831eba7692e4009ab8d029
Joy <joy.xu@students.mq.edu.au>
2025-09-02 14:35:22 +1000

    updated void setup

--- sketch_aug26a/Control_system.ino
+++ sketch_aug26a/Control_system.ino
@@ -4,8 +4,16 @@
 const char* ssid = "network_here"
 const char* password = "password_here"
 
+WifiServer server(80);
+
+String header;
+
+unsigned long currentTime = millis();
+unsigned long prevTime = 0;
+const long timeout = 2000;
+
 void setup() {
-  Serial.begin();
+  Serial.begin(115200);
   pinMode(5,OUTPUT); //may not need
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -14,9 +22,12 @@ void setup() {
     Serial.print('.');
     delay(1000);
   }
+
+  Serial.println("IP Address");
   Serial.println(WiFi.localIP());
+  server.begin();
 }
 
 void loop() {
-
+  
 }
