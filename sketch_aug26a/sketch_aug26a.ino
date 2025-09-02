91c2821006497a63d93b724f940be6ac80a0373f
yshilpakar8 <yshilpakar7@gmail.com>
2025-09-02 15:14:59 +1000

    Wifi code

--- sketch_aug26a/sketch_aug26a.ino
+++ sketch_aug26a/sketch_aug26a.ino
@@ -2,72 +2,84 @@
 //
 
 // sensor pins
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin1 = 9;
+const int echoPin1 = 10;
 
+const int trigPin2 = 5;
+const int echoPin2 = 11;
 
 // h-bridge pins
 const int in1Pin = 13;
 const int forwardPin = 12;
 const int backwardPin = 13;
 
-const int motPin = 11;
-
 const int out1Pin = 2;
 const int out2Pin = 4;
 
+const int motPin = 6;
 int state; // 1 = up 0 = down
 
 // defines variables
-long duration;
-int distance;
-int threshold = 50;
+long duration1;
+int distance1;
+
+long duration2;
+int distance2;
+
+int threshold = 150;
+
 
 
 void setup() {
   pinMode(motPin, OUTPUT);
   pinMode(forwardPin, OUTPUT);
   pinMode(backwardPin, OUTPUT);
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT);
+   pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT);// Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
   state = 0;
 }
 
 void loop() {
+  
+  // Measure first distance
   // Clears the trigPin
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin1, LOW);
   delayMicroseconds(2);
-  
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
+  digitalWrite(trigPin1, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  
+  duration1 = pulseIn(echoPin1, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  distance1 = duration1 * 0.034 / 2;
+
+  delay(50);
   
-  if(distance <= threshold && distance != 0) {
-    digitalWrite(motPin, HIGH);
-    Serial.print("Distance: ");
-    Serial.println(distance);
-  } else if(distance > threshold && && distance != 0) {
-    digitalWrite(motPin, LOW);
-    Serial.print("Distance: ");
-    Serial.println(distance);
-  } 
-  /*
-  //state = 0;
-  if(distance <= threshold && state == 0) {
+  // Measure second distance
+  // Clears the trigPin
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration2 = pulseIn(echoPin2, HIGH);
+  // Calculating the distance
+  distance2 = duration2 * 0.034 / 2;
+
+  
+  if(distance1 <= threshold && state == 0) {
     digitalWrite(forwardPin, LOW);
 	digitalWrite(backwardPin, HIGH);
     delay(5000);
     state = 1;
     
-  } else if(state == 1 && distance > threshold){
+  } else if(state == 1 && distance1 > threshold && distance2 == threshold){
     delay(1000);
     digitalWrite(forwardPin, HIGH);
 	digitalWrite(backwardPin, LOW);
@@ -77,10 +89,11 @@ void loop() {
     digitalWrite(backwardPin, LOW);
     digitalWrite(forwardPin, LOW);
   }
-  */
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.print("Distance1: ");
+  Serial.println(distance1);
+  Serial.print("Distance2: ");
+  Serial.println(distance2);
 }
 
 
