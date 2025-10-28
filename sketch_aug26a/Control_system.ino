9ed1b398562cbb773deeb5e4fe3403538688920f
Roses098 <161680674+Roses098@users.noreply.github.com>
2025-10-28 13:20:26 +1100

    Implement LED control via web interface
    Added support for controlling an LED via a web interface using ESP32. Updated the HTML response to include the current state of the LED and cleaned up unused code.

--- sketch_aug26a/Control_system.ino
+++ sketch_aug26a/Control_system.ino
@@ -1,4 +1,7 @@
 #include <WiFi.h>
+#include <ESP32Servo.h>
+
+const int REDPIN1= 33;
 
 const char* ssid = "ESP32_G34";
 const char* password = "123456789";
@@ -7,32 +10,32 @@ WiFiServer Server(80);
 
 String header;
 
+
+
 unsigned long currentTime = millis();
 unsigned long prevTime = 0;
 const long timeout = 2000;
 
 void setup() {
+  
   Serial.begin(115200);
-  pinMode(5,OUTPUT); //may not need
+  pinMode(REDPIN1, OUTPUT);
+  digitalWrite(REDPIN1,LOW);
+  
   WiFi.mode(WIFI_AP);
-  // WiFi.begin(ssid,password);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   Serial.print('.');
-  //   delay(1000);
-  // }
-  // if (!WiFi.config(local_IP,gateway,subnet)) {
-  //   	Serial.println("STA failed to configure");
-  // }
+  
   WiFi.softAP(ssid, password);
   Serial.print("Connecting to WiFi ..");
 
   Serial.println("IP Address");
   Serial.println(WiFi.softAPIP());
 
+  
+
   Server.begin();
 }
 
-void loop() {
+void loop () {
   WiFiClient client = Server.available();   // Listen for incoming clients
 
   if (client) {                             // If a new client connects,
@@ -46,7 +49,18 @@ void loop() {
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
+        if (c == '\n') {  
+          
+          if (header.indexOf("GET /led?state=1") >= 0) {
+            digitalWrite(REDPIN1, HIGH);
+            //REDPIN1State = "ON";
+          } else if (header.indexOf("GET /led?state=0") >= 0) {
+            digitalWrite(REDPIN1, LOW);
+            //REDPIN1State = "OFF";
+          }
+            
+           String REDPIN1State = digitalRead(REDPIN1) ? "ON" : "OFF"; 
+                             // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {
@@ -56,25 +70,17 @@ void loop() {
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            
-            // // turns the GPIOs on and off - if pins not in use delete ( shows link on website)
-            // if (header.indexOf("GET /26/on") >= 0) {
-            //   Serial.println("GPIO 26 on");
-            //   //output26State = "on";
-            //   digitalWrite(output26, HIGH);
-            // } else if (header.indexOf("GET /26/off") >= 0) {
-            //   Serial.println("GPIO 26 off");
-            //   output26State = "off";
-            //   digitalWrite(output26, LOW);
-            // } else if (header.indexOf("GET /27/on") >= 0) {
-            //   Serial.println("GPIO 27 on");
-            //   output27State = "on";
-            //   digitalWrite(output27, HIGH);
-            // } else if (header.indexOf("GET /27/off") >= 0) {
-            //   Serial.println("GPIO 27 off");
-            //   output27State = "off";
-            //   digitalWrite(output27, LOW);
-            // }
+
+            /*// turning light on and off 
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } */
             
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
@@ -83,10 +89,7 @@ void loop() {
             // CSS to style the on/off buttons 
             // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            //client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            //client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            //client.println(".button2 {background-color: #555555;}</style></head>");
-
+            
             //slider design
             client.println(".switch {position: relative; display: inline-block; width: 100px; height: 54px;}");
             client.println(".slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc;}");
@@ -96,13 +99,10 @@ void loop() {
 
             // Web Page Heading
             client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // divider
-            
-            client.println("<hr>");
-
-            client.println("<h2> Bridge status </h2>");
-            client.println("<section><hr><h4> The lights are currently </h4><h4> The Motor is currently </h4><h4> The bridge is currently </h4></hr></section>");
+                     
+            // divider            
+            client.println("<section><hr><h4> The light is currently " + REDPIN1State + "</h4>");
+            client.println("<h4> The Motor is currently </h4><h4> The bridge is currently </h4></hr></section>");
             client.println("<section><hr><h2> Motor control </h2><label class=\"switch\"><input type=\"checkbox\"><span class=\"slider\"></hr></section>");
             client.println("");
             client.println("");
@@ -110,46 +110,53 @@ void loop() {
 
 
             // Subheading
-
             client.println("<body><h2>Control system</h2>");
             client.println("<body><h3>Boat traffic light #1 </h3>");
 
-            //display slider
-            client.println("<label class=\"switch\"> <input type=\"checkbox\"> <span class=\"slider\"></span></label>");
+            /*//display slider
+            client.println("<label class=\"switch\"> <input type=\"checkbox\" id=\"myCheck\" onchange=\"myFunction()\"");
+            client.println(digitalRead(REDPIN1)?"checked":"");
+            client.println("><span class=\"slider\"></span></label>");
+            client.println("<p id=\"text\" style=\"display:none\">Checkbox is CHECKED!</p>");*/
+
+            client.print("<label class=\"switch\"><input type=\"checkbox\" id=\"myCheck\" onchange=\"myFunction()\" ");
+            if (digitalRead(REDPIN1)) client.print("checked");
+            client.println("><span class=\"slider\"></span></label>");
 
             client.println("<body><h3>Boat traffic light #2 </h3>");
-           client.println("<label class=\"switch\"> <input type=\"checkbox\"> <span class=\"slider\"></span></label>");
-
-            client.println("<body><h3>Car traffic light #2 </h3>");
             client.println("<label class=\"switch\"> <input type=\"checkbox\"> <span class=\"slider\"></span></label>");
 
             client.println("<body><h3>Car traffic light #2 </h3>");
             client.println("<label class=\"switch\"> <input type=\"checkbox\"> <span class=\"slider\"></span></label>");
+
+            client.println("<body><h3>Car traffic light #2 </h3>");
+            client.println("<label class=\"switch\"> <input type=\"checkbox\"> <span class=\"slider\"></span></label>");
+
+
+            client.println("<script>function myFunction() { var xhr = new XMLHttpRequest(); ");
+            client.println("var ledstate = document.getElementById(\"myCheck\").checked?1:0;") ;
+            client.println("xhr.open(\"GET\",\"/led?state=\"+ledstate,true);");
+            client.println("xhr.send();}");
+            //client.println("if (checkBox.checked == true){  text.style.display = \"block\";digitalWrite(REDPIN1,HIGH); } ");
+            client.println("</script>");
 
             
-            // // Display current state, and ON/OFF buttons for GPIO 26  
-            // client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // // If the output26State is off, it displays the ON button       
-            // if (output26State=="off") {
-            //   client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            // } else {
-            //   client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            // } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            // client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // // If the output27State is off, it displays the ON button       
-            // if (output27State=="off") {
-            //   client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            // } else {
-            //   client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            // }
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            /*client.println("<p>GPIO 26 - Green LED: <span class=\"underline\">" + output26State + "</span></p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } */
             client.println("</body></html>");
             
             // The HTTP response ends with another blank line
             client.println();
+
             // Break out of the while loop
             break;
+
           } else { // if you got a newline, then clear currentLine
             currentLine = "";
           }
@@ -165,4 +172,5 @@ void loop() {
     Serial.println("Client disconnected.");
     Serial.println("");
   }
-}
+  
+} 
